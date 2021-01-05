7cbd7e8fac0194f069009db0dbe985acddd5e463
Harald Lissau <s204436@student.dtu.dk>
2021-01-05 11:12:02 +0100

    Created startup.java with basic layout

---
+++ startup.java
@@ -0,0 +1,24 @@
+package minesweeper;
+// This is the main file for Minesweeper. 
+public class startup {
+	
+	public static void main(String[] args) {
+		initializeGame();
+		introduction();
+		mainMenu();
+
+	}
+
+	public static void initializeGame() {
+		
+	}
+	
+	public static void introduction() {
+		
+	}
+	
+	public static void mainMenu() {
+		
+	}
+	
+}
