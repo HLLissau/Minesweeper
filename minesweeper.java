a11eba28a9a442487d6293742f45f99d5d64bd53
Harald Lissau <s204436@student.dtu.dk>
2021-01-07 15:22:38 +0100

    Added minesweeper.java

---
+++ minesweeper.java
@@ -0,0 +1,18 @@
+// Main program file. Contains main and controllor
+public class minesweeper {
+	public static void main(String[] args) {
+		preconfiguration();
+		introduction();
+		mainMenu();
+	}
+	public static void preconfiguration() {
+		
+	}
+	public static void introduction() {
+		
+	}
+	public static void mainMenu() {
+		
+	}
+	
+}
