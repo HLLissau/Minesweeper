f516b10cad5c18568b1c25765f02346af8d58c56
Harald <Harald@192.168.0.41>
2021-01-11 15:59:39 +0100

    Javafx button controls game

--- src/startMinesweeper.java
+++ src/startMinesweeper.java
@@ -1,4 +1,5 @@
 import java.awt.Point;
+import java.awt.event.MouseEvent;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -12,46 +13,13 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 public class startMinesweeper extends Application implements EventHandler<ActionEvent> {
 	
-
-
+	MinesweeperGame game;
+	int spilTilstand;
 	// Main program file. Contains main and controllor
 	
 		
 		public static void main(String[] args) {
 			launch(args);
-			mainMenu();
-		}
-		public static void preconfiguration() {
-			
-		}
-		public static void introduction() {
-			
-		}
-		public static void mainMenu( 	) {
-			MinesweeperGame game = new MinesweeperGame(10, 10, 98);
-			
-			//point size= game.getSize();
-			int spilTilstand = 0;
-			for(;;) {
-				Scanner input= new Scanner(System.in);
-				Point nextTile= getNextInput(game, input);
-				game.getPos(nextTile);
-				//test new pos
-				spilTilstand=game.testConditions(nextTile);
-				if (spilTilstand == 9 || spilTilstand == 8) {
-					break;
-				}
-//				showGameState(game);
-//				System.out.println();
-//				gameState(game);
-			}
-			if (spilTilstand == 8) {
-				victory();
-			}
-			if (spilTilstand == 9) {
-				defeat();
-			}
-			
 		}
 		public static void victory() {
 			System.out.println("Victory Royale!");
@@ -63,12 +31,14 @@ public class startMinesweeper extends Application implements EventHandler<Action
 		
 		@Override
 		public void start(Stage topLevelStage) throws Exception {
+			int spilTilstand = 0;
+			game = new MinesweeperGame(10, 10, 10);
 			topLevelStage.setTitle("This is a title.");
 			StackPane layout = new StackPane();
 			GridPane grid = new GridPane();
-			for (int i =0; i<20; i++) {
-				for (int j =0; j<20; j++) {
-					Button button = new Button();
+			for (int i =0; i<10; i++) {
+				for (int j =0; j<10; j++) {
+					MinesweeperButton button = new MinesweeperButton(j,i);
 					button.setText("" +j +", "+ i);
 					button.setOnAction(this);
 					
@@ -81,7 +51,7 @@ public class startMinesweeper extends Application implements EventHandler<Action
 			
 			System.out.println("End of start method");
 			
-			Scene scene = new Scene(layout, 300,250);
+			Scene scene = new Scene(layout, 350,300);
 			topLevelStage.setScene(scene);
 			topLevelStage.show();
 		
@@ -111,8 +81,41 @@ public class startMinesweeper extends Application implements EventHandler<Action
 
 		@Override
 		public void handle(ActionEvent event) {
-			//if (event.getSource()==button) {
-			//GridPane.
-			//}
+				
+			MinesweeperButton button =(MinesweeperButton)event.getSource();
+			System.out.println(button.getPos());
+			gameState(game);
+			System.out.println();
+			showGameState(game);
+			spilTilstand=game.testConditions(button.getPos());
+			game.getPos(button.getPos());
+			if (spilTilstand == 8) {
+				victory();
+			}
+			if (spilTilstand == 9) {
+				defeat();
+			}
 		}
-}
+
+	
+		
+		
+		
+		public static void showGameState(MinesweeperGame game) {
+			for (int i=game.getSizey();i>0; i--) {
+				for ( int j=0; j<game.getSizex();j++) {
+					System.out.print(game.knownGameState[j][i-1]);
+				}
+			System.out.println();	
+			}
+		}
+		
+		public static void gameState(MinesweeperGame game) {
+			for (int i=game.getSizey();i>0; i--) {
+				for ( int j=0; j<game.getSizex();j++) {
+					System.out.print(game.getGameState()[j][i-1]);
+				}
+			System.out.println();	
+			}
+		}
+	}
